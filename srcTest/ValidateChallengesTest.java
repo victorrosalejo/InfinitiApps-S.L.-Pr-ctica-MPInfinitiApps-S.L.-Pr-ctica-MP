a374ac9319825228251f62069d0b4f67dd10061f
JuliodelJunco <117302342+JuliodelJunco@users.noreply.github.com>
2023-05-12 22:32:12 +0200

    finalisimo
    esta todo

--- srcTest/ValidateChallengesTest.java
+++ srcTest/ValidateChallengesTest.java
@@ -30,7 +30,6 @@ public class ValidateChallengesTest {
         modifiers.setBuff(true);
         modifiers.setName("Manolo");
         modifiers.setValue(2);
-        modifierList.add(modifiers);
         Modifiers modifiers2 = new Modifiers();
 
         List<Modifiers> modifierList2 = new ArrayList<>();
@@ -79,7 +78,7 @@ public class ValidateChallengesTest {
         challengelist.add(challenge1);
         challengelist.add(challenge2);
 
-        String[] entradas = {"1", user2.getRegisterNumber(),"1", "2"};
+        String[] entradas = {"1", user1.getRegisterNumber(),"1", "2"};
 
         String ch = String.join(System.lineSeparator(), entradas) + System.lineSeparator();
 
@@ -87,7 +86,8 @@ public class ValidateChallengesTest {
         System.setIn(in);
 
 
-        databaseP.put(user2.getRegisterNumber(), challengelist);
+        databaseP.put(user1.getRegisterNumber(), challengelist);
+
         databaseManager.saveDatabaseP(databaseP);
         // Validate the challenges
         User aux = new User();
@@ -100,25 +100,25 @@ public class ValidateChallengesTest {
 
 
         // Assert that the challenges are now validated
-        assertEquals(aux2.get(user2.getRegisterNumber()).size(),challengelist.size()-1);
+        assertEquals(aux2.get(user1.getRegisterNumber()).size(),challengelist.size()-1);
 
-        String[] entradas2 = {user2.getRegisterNumber(),"0", "1","0","salir","salir"};
+        String[] entradas2 = {user1.getRegisterNumber(),"0", "1","0","salir","salir"};
 
         ch = String.join(System.lineSeparator(), entradas2) + System.lineSeparator();
 
         in = new ByteArrayInputStream(ch.getBytes());
         System.setIn(in);
+        User aux3 = new User();
 
 
-
-        validator.ValidateChallenges(aux);
-
-
+        validator.ValidateChallenges(aux3);
         aux2 = databaseManager.obtainDatabaseP();
+
+
         Map<String,Character> auxC = databaseManager.obtainDatabaseC();
 
         // Assert that the challenges are now validated
-        assertTrue(aux2.get(user2.getRegisterNumber()).get(0).isValid());
+        assertTrue(aux2.get(user1.getRegisterNumber()).get(0).isValid());
         assertTrue(auxC.get(user1.getRegisterNumber()).getModifiersList().get(0).isActive());
         assertFalse(auxC.get(user2.getRegisterNumber()).getModifiersList().get(0).isActive());
     }
